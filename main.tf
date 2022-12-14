6a76722c99040e89df8b36559a670ca320c5c8e1
William Lee <38011201+nervda@users.noreply.github.com>
2022-12-14 14:16:41 -0500

    create
    initial commit

---
+++ main.tf
@@ -0,0 +1,413 @@
+resource "null_resource" "delay" {
+    provisioner "local-exec" {
+    command = "sleep 1"
+}
+}
+resource "null_resource" "delay2" {
+    provisioner "local-exec" {
+    command = "sleep 1"
+}
+}
+
+resource "null_resource" "delayc" {
+    provisioner "local-exec" {
+    command = "sleep 1"
+}
+}
+
+output "output1" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output2" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output3" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output4" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output5" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output6" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output7" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output8" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output9" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output10" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output11" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output12" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output13" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output14" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output15" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output16" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output17" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output18" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output19" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output20" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output21" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output22" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output23" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output24" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output25" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output26" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output27" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output28" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output29" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output30" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output31" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output32" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output33" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output34" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output35" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output36" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output37" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output38" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output39" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output40" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output41" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output42" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output43" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output44" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output45" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output46" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output47" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output48" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output49" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output50" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output51" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output52" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output53" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output54" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output55" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output56" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output57" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output58" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output59" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output60" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output61" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output62" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output63" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output64" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output65" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output66" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output67" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output68" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output69" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output70" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output71" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output72" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output73" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output74" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output75" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output76" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output77" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output78" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output79" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output80" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output81" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output82" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output83" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output84" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output85" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output86" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output87" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output88" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output89" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output90" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output91" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output92" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output93" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output94" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output95" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output96" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output97" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output98" {
+    description = "make hay"
+    value = "Octave"
+}
+output "output99" {
+    description = "make hay"
+    value = "Octave"
+}
