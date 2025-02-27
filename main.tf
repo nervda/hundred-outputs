3f0f81ecfb4aedb39d2a7c2e6b69d96a7e5bb988
William Lee <38011201+nervda@users.noreply.github.com>
2025-02-27 15:56:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,389 +34,389 @@ output "output2" {
 }
 output "output3" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output4" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output5" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output6" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output7" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output8" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output9" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output10" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output11" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output12" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output13" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output14" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output15" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output16" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output17" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output18" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output19" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output20" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output21" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output22" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output23" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output24" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output25" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output26" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output27" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output28" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output29" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output30" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output31" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output32" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output33" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output34" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output35" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output36" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output37" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output38" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output39" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output40" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output41" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output42" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output43" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output44" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output45" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output46" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output47" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output48" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output49" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output50" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output51" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output52" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output53" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output54" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output55" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output56" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output57" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output58" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output59" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output60" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output61" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output62" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output63" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output64" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output65" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output66" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output67" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output68" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output69" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output70" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output71" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output72" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output73" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output74" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output75" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output76" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output77" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output78" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output79" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output80" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output81" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output82" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output83" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output84" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output85" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output86" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output87" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output88" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output89" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output90" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output91" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output92" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output93" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output94" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output95" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output96" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output97" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output98" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
 output "output99" {
     description = "make hay"
-    value = "Octave"
+    value = var.my-visible-key
 }
